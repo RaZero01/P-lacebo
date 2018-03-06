b20522c7ad9e56644337eebbefc97d7ff071f1c7
Lirveez <lirveez@gmail.com>
2018-03-06 16:52:57 +0300

    New slider for main page

--- src/js/Slider.js
+++ src/js/Slider.js
@@ -1,5 +1,16 @@
-$(window).load(function() {
-    $('.flexslider').flexslider({
-        animation: "slide"
-    });
+$(document).ready(function() {
+    var slides = document.querySelectorAll('#slides .slide');
+    console.log(slides);
+    var currentSlide = 0;
+    var slideInterval = setInterval(nextSlide,4000);
+
+    function nextSlide() {
+        slides[currentSlide].className = 'slide';
+        currentSlide = currentSlide + 1;
+        if (slides.length == currentSlide) {
+            currentSlide = 0;
+        }
+        slides[currentSlide].className = 'slide showing';
+    }
+    nextSlide()
 });
