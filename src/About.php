d712d13b410deab8c59e6f0467049f48c2cfdeff
Ivan Sokolovskii <ivan3177@gmail.com>
2018-03-07 14:38:08 +0300

    Fixed about page template to use common collections template

--- src/About.php
+++ src/About.php
@@ -6,43 +6,43 @@ $blocks = array(
         'url' => 'Daniil.php',
         'name' => 'Даниил Пискарев',
         'img' => 'img/About/Photo1.jpg',
-        'job' => 'Креативный директор'
+        'description' => 'Креативный директор'
     ),
     array(
         'url' => 'Contacts.php',
         'name' => 'Константин Разинков',
         'img' => 'img/About/Photo2.jpg',
-        'job' => 'IT-директор'
+        'description' => 'IT-директор'
     ),
     array(
         'url' => 'Inga.php',
         'name' => 'Инга Крупельницкая',
         'img' => 'img/About/Photo3.jpg',
-        'job' => 'Дизайнер'
+        'description' => 'Дизайнер'
     ),
     array(
         'url' => 'Alena.php',
         'name' => 'Алена Матвеева',
         'img' => 'img/About/Photo4.jpg',
-        'job' => 'Художница'
+        'description' => 'Художница'
     ),
     array(
         'url' => '',
         'name' => 'Анастасия Суравнева',
         'img' => 'img/About/Photo5.jpg',
-        'job' => 'Художница'
+        'description' => 'Художница'
     ),
     array(
         'url' => 'Igor.php',
         'name' => 'Игорь Пискарев',
         'img' => 'img/About/Photo6.jpg',
-        'job' => 'Фотограф'
+        'description' => 'Фотограф'
     ),
     array(
         'url' => 'Andrey.php',
         'name' => 'Андрей Шевченко',
         'img' => 'img/About/Photo7.jpg',
-        'job' => 'Ювелир'
+        'description' => 'Ювелир'
     )
 );
 
