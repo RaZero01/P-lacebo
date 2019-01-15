ad8bebee89a3c6d7eb1ea6b70a2ff0408a6c3a20
Ivan Sokolovskii <ivan3177@gmail.com>
2019-01-15 19:43:36 +0300

    Setup app enviroment and prepare project structure for work

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,17 @@
 const mix = require('laravel-mix');
+require('dotenv').config();
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+mix.js('resources/js/app.js', 'public/js').sass('resources/sass/app.scss', 'public/css').version();
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+let siteName = process.env.APP_NAME; // app name
+let userName = process.env.USER; // developer local username
+
+mix.browserSync({
+    proxy: 'https://' + siteName + '.test',
+    host: siteName + '.test',
+    open: 'external',
+    https: {
+        key: '/Users/' + userName + '/.config/valet/Certificates/' + siteName + '.test.key',
+        cert: '/Users/' + userName + '/.config/valet/Certificates/' + siteName + '.test.crt'
+    }
+});
